c2a2f5ba13dbec8062d00fdcd074d764749e56d3
Wali Haider <109701526+wali15@users.noreply.github.com>
2023-02-05 20:50:41 +0500

    about added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Contact from "./components/Contacts/Contact";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home";
+import About from "./components/About";
 
 const App = () => {
   return (
