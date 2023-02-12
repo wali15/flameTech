44e7e8d25f18f992b3f20101fa90bb727b963b0a
developer001 <developer001@rizsolutions.com>
2023-02-12 15:55:11 +0500

    Added Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Head from "./Head";
+import "./Header.css";
+
+const Header = () => {
+  const [click, setClick] = useState(false);
+
+  return (
+    <>
+      <Head />
+      <header>
+        <nav className="flexSB">
+          <ul
+            className={click ? "mobile-nav" : "flexSB "}
+            onClick={() => setClick(false)}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <div className="start">
+            <div className="button">
+              <a className="fa-solid fa-phone" href="tel:+923363573763">
+                {" "}
+                &nbsp; &nbsp;Call Now
+              </a>
+            </div>
+          </div>
+          <button className="toggle" onClick={() => setClick(!click)}>
+            {click ? (
+              <i className="fa fa-times"> </i>
+            ) : (
+              <i className="fa fa-bars"></i>
+            )}
+          </button>
+        </nav>
+      </header>
+    </>
+  );
+};
+export default Header;
